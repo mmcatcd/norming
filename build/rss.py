5a40eaa1fc0fb8a3c064fa8e5cf287fa0acbbb63
Michael McAndrew <michael@Michaels-MacBook-Pro.local>
2024-11-09 17:49:53 +0000

    More rss cleanup

--- build/rss.py
+++ build/rss.py
@@ -17,11 +17,9 @@ def build_rss(episodes):
       <link>https://normi.ng/{episode.name}</link>
       <description>{episode.description}</description>
       <content:encoded>{episode.description}</content:encoded>
-      <author>{AUTHOR_EMAIL}</author>
       <enclosure url="{episode.audio_file}" length="5484751" type="audio/mpeg"/>
       <guid isPermaLink="true">https://normi.ng/{episode.name}</guid>
       <pubDate>{episode.date.strftime(RSS_DATE_FORMAT)} 09:00:00 GMT</pubDate>
-      <itunes:author>norming</itunes:author>
       {itunes_image}
       <itunes:duration>{episode.duration}</itunes:duration>
       <itunes:summary>{episode.description}</itunes:summary>
@@ -30,7 +28,7 @@ def build_rss(episodes):
     )
 
   rss_string = inspect.cleandoc(f"""<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atomic="http://atomicpublishing.com/rss/1.0/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:spotify="http://www.spotify.com/ns/rss">
+  <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:anchor="https://anchor.fm/xmlns">
     <channel>
       <title>Norming</title>
       <link>https://normi.ng</link>
@@ -47,7 +45,7 @@ def build_rss(episodes):
         <title>Norming</title>
         <link>https://normi.ng</link>
       </image>
-      <atom:link href="https://normi.ng/rss.xml" type="application/rss+xml" rel="self"></atom:link>
+      <atom:link href="https://normi.ng/rss.xml" type="application/rss+xml" rel="self"/>
       <itunes:author>Michael McAndrew</itunes:author>
       <itunes:image href="https://normi.ng/img/coverart.jpg"/>
       <itunes:owner>
